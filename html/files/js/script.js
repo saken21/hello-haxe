cd8f2dc2011b9ceec982a8c1d6de2b581d180ec1
Sakata <sakata@graphic.co.jp>
2015-07-31 15:44:45 +0900

    Test

--- html/files/js/script.js
+++ html/files/js/script.js
@@ -2,18 +2,8 @@
 var src = {};
 src.Main = function() { };
 src.Main.main = function() {
-	var array = ["test","test2"];
-	var func = function() {
-		console.log("func");
-	};
-	var _g1 = 0;
-	var _g = array.length;
-	while(_g1 < _g) {
-		var p = _g1++;
-		console.log(array[p]);
-	}
-	window.alert("test");
-	func();
+	var all = window.document.getElementById("all");
+	all.innerText = "taa";
 };
 src.Main.main();
 })();
--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,14 @@
 package src;
 
 import js.Browser;
+import js.html.Element;
  
 class Main {
 	
 	public static function main():Void {
+		
+		var all:Element = Browser.document.getElementById('all');
+		all.innerText = 'taa';
 		
     }
 
