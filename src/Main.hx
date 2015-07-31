9f23140d56afa3d7b01272a2792b4fc61849aea6
okawa <ookawa124@gmail.com>
2015-07-31 15:35:22 +0900

    firstcommit:7031

--- src/Main.hx
+++ src/Main.hx
@@ -1,11 +1,21 @@
 package src;
 
 import js.Browser;
+import js.JQuery;
  
 class Main {
 	
 	public static function main():Void {
+		var newText:String = 'Helllo-Haxe';
+		var element = Browser.document.getElementsByTagName('p');
+		trace(untyped element);
+
+    new JQuery( 'p' ).text( newText );
+    new JQuery( 'p' ).click( function() { 
+      JQuery.cur.text( 'change' );
+    });
+
 		
-    }
+  }
 
 }
